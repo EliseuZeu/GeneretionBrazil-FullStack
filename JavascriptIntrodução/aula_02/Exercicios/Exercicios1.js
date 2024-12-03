5fb53cbd5835613608f7d140e3409cfac82706e4
EliseuZz <100594790+EliseuZeu@users.noreply.github.com>
2024-12-03 14:22:29 -0300

    Exercicio 1

---
+++ JavascriptIntrodução/aula_02/Exercicios/Exercicios1.js
@@ -0,0 +1,14 @@
+const leia = require('readline-sync')
+
+let salario, abono
+
+
+salario = leia.questionFloat("Digite o salario seu salario: "); // variavel que recebe a entrada do usuario no caso salario
+abono = leia.questionFloat("Digite o Abono salarial: ") // variavel que recebe a entrada do usuario no caso Abono
+
+function calcularNovoSalario(salario , abono){  // função retorna o valor do salario e abono calculado retornando o novo salario de acordo com novos parametros
+    return novoSalario = salario + abono; // retorno da função novo salario
+}
+
+console.log("Seu novo salario e: ", calcularNovoSalario(salario,abono)); // saida de dos dados do novo salario.
+
